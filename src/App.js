33119c746605d10feed4cd502a4c4c7eab5e4ac0
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-03-06 17:16:50 -0300

    CSS Finalizado
    Toda parte de CSS finalizado;
    
    E começado toda a lógica da aplicação

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './style.css';
 
 function App() {
+
+  const [input, setInput] = useState("")
+
+  function handleSearch() {
+    alert(`Valor do input: ${input}`)
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
@@ -10,9 +18,11 @@ function App() {
         <input
           type="text"
           placeholder="Digite seu cep..."
+          value={input}
+          onChange={e => setInput(e.target.value)}
         />
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#fff" />
         </button>
       </div>
