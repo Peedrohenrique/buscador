4a34efdf2af3873cf4aaf8f00d6efaa3d8cb5cbe
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2022-03-06 22:10:40 -0300

    Finalizado
    Depois Vou fazer algumas melhoria,

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './style.css';
+import Api from './services/api'
 
 function App() {
 
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
 
-  function handleSearch() {
-    alert(`Valor do input: ${input}`)
+  async function handleSearch() {
+    if (input === '') {
+      alert("Preencha algum CEP!") //Colocador dentro do state depois
+      return;
+    }
+    try {
+      const res = await Api.get(`${input}/json`)
+      setCep(res.data)
+      setInput("");
+    } catch {
+      alert("Ops.. CEP errado!!!") //Colocador dentro do state depois
+      setInput("")
+    }
   }
 
   return (
@@ -27,14 +40,19 @@ function App() {
         </button>
       </div>
 
-      <main className='main'>
-        <h2>CEP: 978445184</h2>
+      {Object.keys(cep).length > 0 && ( //Renderização condicional
+        <main className='main'>
+          <h2>CEP: {cep.cep}</h2>
 
-        <span>Rua  São francisco</span>
-        <span>Complemento: Algum complemento</span>
-        <span>Santo Amaro</span>
-        <span>Fortaleza - CE</span>
-      </main>
+          <span>{cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>ddd: {cep.ddd}</span>
+          <span>Siafi: {cep.siafi}</span>
+          <span>Ibge: {cep.ibge}</span>
+          <span>Bairro: {cep.bairro}</span>
+          <span>Localidade: {cep.localidade}-{cep.uf}</span>
+        </main>
+      )}
     </div>
   );
 }
